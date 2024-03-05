91e7a5cbf173588b75476fa03d8f2e1ba748c34a
Marcelo <marceloferraira@hotmail.com>
2024-03-04 21:51:24 -0300

    update

--- 3° Período/Alisson_Estrutura Dados II/env/busca_ibge.py
+++ 3° Período/Alisson_Estrutura Dados II/env/busca_ibge.py
@@ -1,17 +1,25 @@
 from flask import Flask,request
-
+from ibge import busca
 #instanciar a aplicaçao 
 app = Flask(__name__)
 
 
 @app.route("/busca_nome")
-def busca():
+def busca_nome():
     try: #tente
+        #http://127.0.0.1:5000/busca_nome?nome=Marcelo
         var_nome = request.args.get("nome")
-        return var_nome
+        response = busca(var_nome)
+        
+        #Percorrer o retorno e somar todas as ocorrencias do nome 
+
+        return response
+
 
     except Exception as e: #se não #e nome do erro
         return f"Falha na rota /busca_nome: {e}"
+   
+
 
 #debug == True para nao ser necessario recarregar a app no navegador, por
 app.run(debug=True)
