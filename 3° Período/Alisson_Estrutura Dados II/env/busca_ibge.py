a05918a8502145eaeab17cbadef74044dc7d0edd
Marcelo <marceloferraira@hotmail.com>
2024-03-11 21:28:28 -0300

    update

--- 3° Período/Alisson_Estrutura Dados II/env/busca_ibge.py
+++ 3° Período/Alisson_Estrutura Dados II/env/busca_ibge.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-from ibge import busca
+from ibge import busca, calcula_ocorrencias, lista
 #instanciar a aplicaçao 
 app = Flask(__name__)
 
@@ -10,16 +10,58 @@ def busca_nome():
         #http://127.0.0.1:5000/busca_nome?nome=Marcelo
         var_nome = request.args.get("nome")
         response = busca(var_nome)
-        
+        soma = calcula_ocorrencias(response)
+
+        objeto_retorno = {
+            "nome_procurado": var_nome,
+            "total_ocorrencia": soma
+        }
+        return objeto_retorno
         #Percorrer o retorno e somar todas as ocorrencias do nome 
 
-        return response
+       
 
 
     except Exception as e: #se não #e nome do erro
         return f"Falha na rota /busca_nome: {e}"
    
+@app.route("/frequencia_min")
+def minimo():
+    try:
+        nome = request.args.get("nome")
+        response = busca(nome)
+        data, qtd = lista(response)
+        menor_numero = min(qtd)
+        periodo = data[qtd.index(menor_numero)]
 
+        retorno = {
+            "Nome procurado": nome,
+            "Periodo":  periodo,
+            "Frequencia": menor_numero
+        }
+        return retorno
+
+    except Exception as e:
+        return f"Falha na rota /frequencia_min: {e}"
+    
+@app.route("/frequencia_max")
+def maximo():
+    try:
+        nome = request.args.get("nome")
+        response = busca(nome)
+        data, qtd = lista(response)
+        maior_numero = max(qtd)
+        periodo = data[qtd.index(maior_numero)]
+
+        retorno = {
+            "Nome procurado": nome,
+            "Periodo":  periodo,
+            "Frequencia": maior_numero
+        }
+        return retorno
+
+    except Exception as e:
+        return f"Falha na rota /frequencia_max: {e}"
 
 #debug == True para nao ser necessario recarregar a app no navegador, por
 app.run(debug=True)
--- 3° Período/Alisson_Estrutura Dados II/env/ibge.py
+++ 3° Período/Alisson_Estrutura Dados II/env/ibge.py
@@ -1,4 +1,4 @@
-import requests
+import requests #faz a requisição de algum endereço on-line
 
 
 
@@ -6,6 +6,28 @@ def busca(nome):
     url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}"
     resposta = requests.get(url)
     return resposta.json()
+
+def calcula_ocorrencias(json):
+    conteudo = json[0]
+    resposta = conteudo.get("res", []) #tenta acessar a chave res, se não achar devolve uma lista vazia
+    soma = []
+    for elemento in resposta:
+        frequencia = elemento.get("frequencia", 0) #pega a chave frequencia, se não tiver retorna 0 para tratar erros
+        soma.append(frequencia)
+
+    return sum(soma)
+        
+def lista(json):
+    conteudo = json[0]
+    resposta = conteudo.get("res", [])
+    periodo = []
+    frequencia = []
+    for elemento in resposta:
+        data = elemento.get("periodo", 0)
+        qtd = elemento.get("frequencia", 0)
+        periodo.append(data)
+        frequencia.append(qtd)
+    return periodo, frequencia
 
 
 #teste = busca("Jose")
