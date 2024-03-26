5bc6b51f6390d6c5b483bb577f4f61173d4dc31f
Marcelo <marceloferraira@hotmail.com>
2024-03-25 21:47:47 -0300

    update

---
+++ 3° Período/Alisson_Estrutura Dados II/env/Projeto01/ibge.py
@@ -0,0 +1,13 @@
+from flask import Flask,request
+from funcoes import lista
+
+app = Flask(__name__)
+
+@app.route("/distrito")
+def distrito():
+    try:
+        retorno = lista()
+        return retorno
+    
+    except Exception as e:
+        return f"Falha na rota /distrito: {e}"
