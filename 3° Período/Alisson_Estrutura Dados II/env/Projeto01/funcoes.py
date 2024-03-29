21d36e7754539c4f1493cc8268eedcdec4d30c2d
Marcelo <marceloferraira@hotmail.com>
2024-03-28 23:01:39 -0300

    update

--- 3° Período/Alisson_Estrutura Dados II/env/Projeto01/funcoes.py
+++ 3° Período/Alisson_Estrutura Dados II/env/Projeto01/funcoes.py
@@ -1,15 +1,42 @@
 import requests
+import unicodedata
 
-def lista():
+#função para remover os acentos
+def remover_acentos(texto):
+    return ''.join(c for c in unicodedata.normalize('NFD', texto)
+                   if unicodedata.category(c) != 'Mn')
+
+
+def lista_cidades():
     url = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
     resposta = requests.get(url)
     return resposta.json()
 
 def nomes_cidades(lista):
-    teste = lista()
-    for i in range (0,len(teste)):
-        conteudo = lista[i]["id"]
-        print (conteudo)
-        teste.append = conteudo.get("nome", [])
-    print (teste)
-    return teste
+    cidades = []
+    for cidade in lista:
+        #nome recebe o nome da cidade, se não tiver nada rebebe None
+        nome = cidade.get("nome", None)
+        #verifica so a variavel nome nao esta vazia, se nao estiver adiciona o nome no array teste
+        if nome != None:
+            cidades.append(nome)
+    #cria a lista sem acentuação   
+    cidades_sem_acentos= [remover_acentos(verificar) for verificar in cidades]
+        
+    return cidades_sem_acentos
+
+def ordernar_cidades(json):
+    #chama a funcao nomes_cidade
+    lista = nomes_cidades(json)
+    #organiza por ordem alfabetica
+    lista.sort
+    ordenada = sorted(lista)
+
+    return ordenada
+
+def busca_estado(estado):
+    url = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{estado}/distritos"
+    resposta = requests.get(url)
+    #retorna a lista em json
+    return resposta.json()
+
--- 3° Período/Alisson_Estrutura Dados II/env/Projeto01/ibge1.py
+++ 3° Período/Alisson_Estrutura Dados II/env/Projeto01/ibge1.py
@@ -1,20 +1,28 @@
 from flask import Flask,request
-from funcoes import lista, nomes_cidades
+from funcoes import lista_cidades, ordernar_cidades, busca_estado
 
 app = Flask(__name__)
 
-@app.route("/distrito")
+#ROta para buscar todas cidades do pais por ordem alfabetica
+@app.route("/distritos_ordenados")
 def distrito():
     try:
-        retorno = lista()
-        teste = nomes_cidades(retorno)
-        print (teste)
-        return teste
-    
+        json = lista_cidades()
+        resposta = ordernar_cidades(json)
+        return resposta
     except Exception as e:
-        return f"Falha na rota /distrito: {e}"
-    
+        return f"Falha na rota /distritos_ordenados: {e}"
 
-
+#rota para mostrar todas cidades do estado em ordem alfabetica
+#nocessario passar    
+@app.route("/cidades_do_estado")
+def estados():
+    try:
+        estado = request.args.get("estado")
+        json = busca_estado(estado)
+        resposta = ordernar_cidades(json)
+        return resposta
+    except Exception as e:
+        return f"Falha na rota /cidades_do_estado: {e}"
     
 app.run(debug=True)
